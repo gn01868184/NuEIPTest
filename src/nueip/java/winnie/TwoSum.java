c23c142c0282c21169f074245291828092b57a47
gn01868184 <gn01868184@gmail.com>
2021-08-15 09:01:12 +0800

    fix: Single Loop to Double Loop

--- src/nueip/java/winnie/TwoSum.java
+++ src/nueip/java/winnie/TwoSum.java
@@ -12,10 +12,12 @@ public class TwoSum {
 	public static int[] towSum(int[] nums, int target) {
 		int[] output = new int[2];
 		for (int i = 0; i < nums.length - 1; i++) {
-			if ((nums[i] + nums[i + 1]) == target) {
-				output[0] = i;
-				output[1] = i + 1;
-				break;
+			for (int j = i + 1; j < nums.length; j++) {
+				if ((nums[i] + nums[j]) == target) {
+					output[0] = i;
+					output[1] = j;
+					return output;
+				}
 			}
 		}
 		return output;
